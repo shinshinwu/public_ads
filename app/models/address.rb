2ad5401d06894a879b099af7ead4b400282348f6
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-03-20 23:00:50 -0700

    FEATURE: completed back-end for radius and category search from home->index views. need to move on to front end styling for search view and adding search abilities on index view.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,13 @@
 class Address < ActiveRecord::Base
+include PgSearch
   belongs_to :listing
   geocoded_by :full_street_address
   after_validation :geocode
+
+
+  pg_search_scope :address_search, :associated_against => {
+    :listing => :category
+  }
 
   def full_street_address
   	if line_1.present? && zipcode.present? || city.present?
