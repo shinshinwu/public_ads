0261964054bee4c026e878d48de700f37201baf8
Anna Wu <shinshinwu@gmail.com>
2016-04-04 22:48:13 -0700

    change default img url

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
   has_many   :messages
   has_many   :transactions
 
-  has_attached_file :photo, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, default_url: "publicad-logo-lg.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   # validates_attachment_presence :photo
